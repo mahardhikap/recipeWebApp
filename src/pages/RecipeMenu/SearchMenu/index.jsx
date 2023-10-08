509faa6ad5fc816fe62c3f2d4785c882994729cf
Mahardhika Pratama <putrad578@gmail.com>
2023-10-08 22:31:15 +0700

    restyle search, filter

--- src/pages/RecipeMenu/SearchMenu/index.jsx
+++ src/pages/RecipeMenu/SearchMenu/index.jsx
@@ -5,10 +5,11 @@ import NavbarCustom from '../../../components/Navbar';
 import Footer from '../../../components/Footer';
 import { getSearchSort } from '../../../redux/actions/menu';
 import { useDispatch, useSelector } from 'react-redux';
+import Swal from 'sweetalert2';
 
 function SearchMenu() {
   const dispatch = useDispatch()
-  const {data} = useSelector(state => state.getSearchSort)
+  const {data, isError, errorMessage} = useSelector(state => state.getSearchSort)
   const [sortby, setSortby] = useState('title');
   const [sort, setSort] = useState('ASC');
   const [page, setPage] = useState(1);
@@ -16,6 +17,15 @@ function SearchMenu() {
   const [search, setSearch] = useState('');
   const [searchby, setSearchby] = useState('title');
   let token = localStorage.getItem("token")
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const selectedButtonStyle = {
+    backgroundColor: '#00E092',
+  };
+  
+  const unselectedButtonStyle = {
+    backgroundColor: '#EFC81A',
+  };
 
   const getData = () => {
     dispatch(getSearchSort(searchby, search, sortby, sort, page, limit))
@@ -29,12 +39,37 @@ function SearchMenu() {
     }
   }
 
+  const onChangeSearchBy = (e) => {
+    setSearchby(e.target.value);
+  };
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const onChangeSortBy = (e) => {
+    setSortby(e.target.value);
+  };
+
+  const onChangeSort = (e) => {
+    setSort(e.target.value);
+  };
+
+  const onChangeLimit = (e) => {
+    setLimit(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPage(1);
+    getData();
+  };
+
   const goToPage = pageNumber => {
     if (pageNumber >= 1 && pageNumber <= data?.pages.totalPage) {
       setPage(pageNumber);
     }
   };
-
 
   useEffect(()=>{
     getData()
@@ -52,13 +87,15 @@ function SearchMenu() {
       )} */}
         <h1 className="text-purple">Discover Recipe & Delicious Food</h1>
         <div>
-          <form className="row gap-3">
+          <form className="row gap-3" onSubmit={handleSubmit}>
             <div className="col-sm-12 col-md-6 col-lg-6">
               <input
                 type="text"
                 name="search"
                 placeholder="Telur Gulung"
                 className="p-3 rounded bg-body-secondary border-0 w-100 form-control"
+                onChange={onChangeSearch}
+                value={search}
               />
             </div>
             <div className="col-sm-12 col-md-3 col-lg-3">
@@ -71,51 +108,96 @@ function SearchMenu() {
             </div>
           </form>
         </div>
-        <div className='change-limit d-flex gap-2 my-2 flex-wrap'>
-          <div>Show</div>
-          <select name="limit" id="lmt" className='border-0 bg-warning rounded'>
-            <option value="1">1</option>
-            <option value="3">3</option>
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="1000">All</option>
-          </select>
-          <div>Search By</div>
-          <select name="searchby" id="sby" className='border-0 bg-warning rounded'>
-            <option value="username">Username</option>
-            <option value="title">Title</option>
-            <option value="ingredients">Ingredients</option>
-            <option value="category.name">Category</option>
-          </select>
-          <div>Order</div>
-          <select name="order" id="ord" className='border-0 bg-warning rounded'>
-            <option value="ASC">A-Z</option>
-            <option value="DESC">Z-A</option>
-          </select>
-          <div>Order By</div>
-          <select name="orderby" id="oby" className='border-0 bg-warning rounded'>
-            <option value="created_at">Created</option>
-            <option value="title">Title</option>
-            <option value="username">Username</option>
-          </select>
+        <div className='d-flex gap-2 my-2 flex-wrap'>
+          <div  className='border-0 bg-warning rounded d-flex p-2 gap-2 text-white'>
+            <div>Show</div>
+            <select name="limit" id="lmt" className='bg-transparent border-2 rounded text-white border-white' onInput={onChangeLimit} value={limit}>
+              <option value="2" className='bg-warning'>2</option>
+              <option value="4" className='bg-warning'>4</option>
+              <option value="8" className='bg-warning'>8</option>
+            </select>
+          </div>
+          <div className='border-0 bg-warning rounded d-flex p-2 gap-2 text-white'>
+            <div>Search By</div>
+            <select name="searchby" id="sby" className='bg-transparent border-2 rounded text-white border-white' onInput={onChangeSearchBy} value={searchby}>
+              <option value="username" className='bg-warning'>Username</option>
+              <option value="title" className='bg-warning'>Title</option>
+              <option value="category.name" className='bg-warning'>Category</option>
+            </select>
+          </div>
+          <div className='border-0 bg-warning rounded d-flex p-2 gap-2 text-white'>
+            <div>Order</div>
+            <select name="order" id="ord" className='bg-transparent border-2 rounded text-white border-white' onInput={onChangeSort} value={sort}>
+              <option value="ASC" className='bg-warning'>A-Z</option>
+              <option value="DESC" className='bg-warning'>Z-A</option>
+            </select>
+          </div>
+          <div className='border-0 bg-warning rounded d-flex p-2 gap-2 text-white'>
+            <div>Order By</div>
+            <select name="orderby" id="oby" className='bg-transparent border-2 rounded text-white border-white' onInput={onChangeSortBy} value={sortby}>
+              <option value="created_at" className='bg-warning'>Created</option>
+              <option value="title" className='bg-warning'>Title</option>
+              <option value="username" className='bg-warning'>Username</option>
+            </select>
+          </div>
         </div>
         <div className="mt-3 d-flex gap-3 flex-wrap">
-          <button className="rounded p-1 border-0 text-white bg-warning">
+        <button
+            className="rounded px-3 py-2 border-0 text-white"
+            style={
+              selectedCategory === 'New'
+                ? selectedButtonStyle
+                : unselectedButtonStyle
+            }
+            onClick={() => {
+              setSelectedCategory('New');
+            }}
+          >
             New
           </button>
-          <button className="rounded p-1 border-0 text-white bg-warning">
-            Popular
+          <button
+            className="rounded px-3 py-2 border-0 text-white"
+            style={
+              selectedCategory === 'Appetizer'
+                ? selectedButtonStyle
+                : unselectedButtonStyle
+            }
+            onClick={() => {
+              setSelectedCategory('Appetizer');
+            }}
+          >
+            Appetizer
           </button>
-          <button className="rounded p-1 border-0 text-white bg-success">
-            Vegetarian
+          <button
+            className="rounded px-3 py-2 border-0 text-white"
+            style={
+              selectedCategory === 'Main Course'
+                ? selectedButtonStyle
+                : unselectedButtonStyle
+            }
+            onClick={() => {
+              setSelectedCategory('Main Course');
+            }}
+          >
+            Main Course
           </button>
-          <button className="rounded p-1 border-0 text-white bg-success">
-            Breakfast
+          <button
+            className="rounded px-3 py-2 border-0 text-white"
+            style={
+              selectedCategory === 'Dessert'
+                ? selectedButtonStyle
+                : unselectedButtonStyle
+            }
+            onClick={() => {
+              setSelectedCategory('Dessert');
+            }}
+          >
+            Dessert
           </button>
         </div>
-        {data?.rows?.map((item) =>{
+        {data?.rows?.map((item, index) =>{
           return(
-              <div className="row mt-5 align-items-center">
+              <div className="row mt-5 align-items-center" key={index}>
                 <div className="col-sm-12 col-md-6 col-lg-4">
                   <Link
                     to={`/detail-menu/${item.id}`}
@@ -126,8 +208,8 @@ function SearchMenu() {
                       alt="photo-menu"
                       className="img-thumbnail ratio ratio-1x1"
                       style={{
-                        width: '300px',
-                        height: '300px',
+                        // width: '300px',
+                        height: '340px',
                         objectFit: 'cover',
                       }}
                     />
@@ -146,14 +228,14 @@ function SearchMenu() {
                   <div className="w-100">
                     <div className="bg-warning rounded p-1 text-center d-flex justify-content-evenly fw-bold">
                       <div className='d-flex justify-content-center align-items-center text-white'>
-                        <i class="bi bi-hand-thumbs-up-fill fs-4 btn text-white"></i>
-                        {item.like_count} LIKE
+                        <i className="bi bi-hand-thumbs-up-fill fs-4 btn text-white"></i>
+                        {item.like_count}
                       </div>
                       <div>
-                        <i class="bi bi-chat-left-text-fill fs-4 btn text-white"></i>
+                        <i className="bi bi-chat-left-text-fill fs-4 btn text-white"></i>
                       </div>
                       <div>
-                        <i class="bi bi-bookmark-fill fs-4 btn text-white"></i>
+                        <i className="bi bi-bookmark-fill fs-4 btn text-white"></i>
                       </div>
                     </div>
                   </div>
